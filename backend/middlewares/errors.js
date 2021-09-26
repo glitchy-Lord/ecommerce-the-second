b92dac194927e42cacde1ba6e9d70376380e8a82
Bhushan Alhat <alhatbhushan@gmail.com>
2021-09-26 18:21:33 +0530

    error handling for duplicate email when registering, JWT invalid and expired error

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -27,6 +27,24 @@ module.exports = (err, req, res, next) => {
 			err = new ErrorHandler(message, 400);
 		}
 
+		//* Handling Mongoose duplicate key error
+		if (err.code === 11000) {
+			const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+			err = new ErrorHandler(message, 400);
+		}
+
+		//* Handling wrong JWT error
+		if (err.name === 'JsonWebTokenError') {
+			const message = 'JSON Web Token is invalid. Try Again.';
+			err = new ErrorHandler(message, 400);
+		}
+
+		//* Handling expired JWT error
+		if (err.name === 'TokenExpiredError') {
+			const message = 'JSON Web Token is expired. Try Again.';
+			err = new ErrorHandler(message, 400);
+		}
+
 		res.status(err.statusCode).json({
 			success: false,
 			error: err.message,
