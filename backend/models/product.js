fbd11c3b2ae1b09cb44ceafb6d571b5c6cd4e3f9
Bhushan Alhat <alhatbhushan@gmail.com>
2021-10-02 12:33:31 +0530

    added orders model

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,82 +1,58 @@
 const mongoose = require('mongoose');
 
-const productSchema = new mongoose.Schema({
+const ProductSchema = new mongoose.Schema({
 	name: {
 		type: String,
 		required: [true, 'Enter a product name'],
 		trim: true,
 		maxLength: [100, 'Product name cannot exceed 100 characters'],
 	},
+
 	price: {
 		type: Number,
 		required: [true, 'Enter a price'],
 		maxLength: [100, 'Product name cannot exceed 100 characters'],
 		default: 0.0,
 	},
+
 	description: {
 		type: String,
 		required: [true, 'Enter a product description'],
 		trim: true,
 	},
-	ratings: {
-		type: Number,
-		default: 0,
-	},
+
+	ratings: { type: Number, default: 0 },
+
 	images: [
 		{
-			public_id: {
-				type: String,
-				required: true,
-			},
-			url: {
-				type: String,
-				required: true,
-			},
+			public_id: { type: String, required: true },
+			url: { type: String, required: true },
 		},
 	],
-	category: {
-		type: String,
-		required: [true, 'Select a product category'],
-	},
-	seller: {
-		type: String,
-		required: [true, 'Enter a seller name'],
-	},
+
+	category: { type: String, required: [true, 'Select a product category'] },
+
+	seller: { type: String, required: [true, 'Enter a seller name'] },
+
 	stock: {
 		type: Number,
 		required: [true, 'Enter product stock'],
 		maxLength: [5, 'Product stock cannot exceed 99999'],
 		default: 0,
 	},
-	numOfReviews: {
-		type: Number,
-		default: 0,
-	},
+
+	numOfReviews: { type: Number, default: 0 },
+
 	reviews: [
 		{
-			name: {
-				type: String,
-				required: true,
-			},
-			rating: {
-				type: Number,
-				required: true,
-			},
-			comment: {
-				type: String,
-				required: true,
-			},
+			name: { type: String, required: true },
+			rating: { type: Number, required: true },
+			comment: { type: String, required: true },
 		},
 	],
-	user: {
-		type: mongoose.Schema.ObjectId,
-		ref: 'User',
-		required: true,
-	},
-	createdAt: {
-		type: Date,
-		default: Date.now,
-	},
-});
 
-module.exports = mongoose.model('Product', productSchema);
+	user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+
+	createdAt: { type: Date, default: Date.now },
+});
+module.exports = mongoose.model('Product', ProductSchema);
--- backend/models/user.js
+++ backend/models/user.js
@@ -10,37 +10,32 @@ const userSchema = new mongoose.Schema({
 		required: [true, 'Enter your name'],
 		maxLength: [30, 'Name cannot exceed 30 characters'],
 	},
+
 	email: {
 		type: String,
 		required: [true, 'Enter your email'],
 		unique: true,
 		validate: [validator.isEmail, 'Enter a valid email address'],
 	},
+
 	password: {
 		type: String,
 		required: [true, 'Enter your password'],
 		minLength: [6, 'Password must be longer than 6 characters'],
 		select: false,
 	},
+
 	avatar: {
-		public_id: {
-			type: String,
-			required: true,
-		},
-		url: {
-			type: String,
-			required: true,
-		},
+		public_id: { type: String, required: true },
+		url: { type: String, required: true },
 	},
-	role: {
-		type: String,
-		default: 'user',
-	},
-	createdAt: {
-		type: Date,
-		default: Date.now,
-	},
+
+	role: { type: String, default: 'user' },
+
+	createdAt: { type: Date, default: Date.now },
+
 	resetPasswordToken: String,
+
 	resetPasswordExpire: Date,
 });
 
@@ -49,7 +44,6 @@ userSchema.pre('save', async function (next) {
 	if (!this.isModified('password')) {
 		next();
 	}
-
 	this.password = await bcrypt.hash(this.password, 10);
 });
 
