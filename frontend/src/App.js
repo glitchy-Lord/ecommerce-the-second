2d6ebfd1c6fc8d72dcd4bc15e11a4e62d6f0fa32
Bhushan Alhat <alhatbhushan@gmail.com>
2021-10-10 23:48:58 +0530

    Added homepage & react-router & custom titles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
+import Home from './components/Home';
+import { Route } from 'react-router-dom';
 
 import './App.css';
 
@@ -7,6 +9,9 @@ function App() {
 	return (
 		<div className='App'>
 			<Header />
+			<div className='container container-fluid'>
+				<Route path='/' component={Home} exact />
+			</div>
 			<Footer />
 		</div>
 	);
