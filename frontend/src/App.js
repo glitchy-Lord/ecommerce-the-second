1a3631983f8ac4e44e0f47118a249ce476197ac4
Bhushan Alhat <alhatbhushan@gmail.com>
2021-10-10 16:35:07 +0530

    added layout comp (header and footer)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+
 import './App.css';
 
 function App() {
 	return (
 		<div className='App'>
-			<h1>eCommerce the second</h1>
+			<Header />
+			<Footer />
 		</div>
 	);
 }
